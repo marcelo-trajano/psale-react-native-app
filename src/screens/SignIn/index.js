617118e11f9e540ce7ec1cab6733be4e4a221841
Marcelo <marcelo.trajano@outlook.com>
2020-10-01 23:54:10 -0300

    update

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SighMessageButton,
+  SighMessageButtonText,
+  SighMessageButtonTextBold,
+} from './styles';
 import BarberLogo from '../../assets/barber.svg';
 
 export default () => {
   return (
     <Container>
       <BarberLogo width="100%" height="160" />
+      <InputArea>
+        <CustomButton>
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+      <SighMessageButton>
+        <SighMessageButtonText>
+          Ainda nao possui conta?
+          <SighMessageButtonTextBold>Cadastre-se</SighMessageButtonTextBold>
+        </SighMessageButtonText>
+      </SighMessageButton>
     </Container>
   );
 };
--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -7,3 +7,28 @@ export const Container = styled.SafeAreaView`
   justify-content: center;
   align-items: center;
 `;
+
+export const InputArea = styled.View`
+  padding: 40px;
+`;
+
+export const CustomButton = styled.TouchableOpacity`
+  background-color: #268596;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+`;
+
+export const CustomButtonText = styled.Text`
+  background-color: #63c2d1;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SighMessageButton = styled.TouchableOpacity``;
+
+export const SighMessageButtonText = styled.Text``;
+
+export const SighMessageButtonTextBold = styled.Text``;
