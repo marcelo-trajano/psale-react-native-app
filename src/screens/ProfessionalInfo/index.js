e8c021587d45238fb5e2ee6b063e7638fd7920f9
Marcelo <marcelo.trajano@outlook.com>
2020-10-04 23:46:11 -0300

    update

--- src/screens/ProfessionalInfo/index.js
+++ src/screens/ProfessionalInfo/index.js
@@ -1,8 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import {Text} from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  LoadingIcon,
+  Scroller,
+  EmptyHeader,
+  PageBody,
+  UserInfoArea,
+  ServiceArea,
+  TestimonialArea,
+  SwipeDot,
+  SwipeDotActive,
+  SwiperItem,
+  SwipperImage,
+  UserAvatar,
+  UserInfo,
+  UserName,
+  FavoriteButtom,
+} from './styles';
+import Stars from '../../components/Stars';
+import FavoriteIcon from '../../assets/favorite.svg';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Swipper from 'react-native-swiper';
 import Api from '../../Api';
 
 export default () => {
@@ -12,19 +32,64 @@ export default () => {
   const [userInfo, setuserInfo] = useState({
     id: route.params.id,
   });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getInfoProfessional = async () => {
+      setLoading(true);
       const token = await AsyncStorage.getItem('token');
+
       const info = await Api.getProfessional(userInfo.id, token);
-      console.log(info);
+
+      if (info.error === '') {
+        setuserInfo(info.data);
+      } else {
+        alert('error:' + info.error);
+      }
+      setLoading(false);
     };
     getInfoProfessional();
   }, []);
 
   return (
     <Container>
-      <Text>ProfessionalInfo</Text>
+      <Scroller>
+        {userInfo.photos && userInfo.photos.length > 0 ? (
+          <Swipper
+            style={{height: 240}}
+            dot={<SwipeDot />}
+            activeDot={<SwipeDotActive />}
+            paginationStyle={{top: 15, right: 15, bottom: null, left: null}}
+            autoplay={true}>
+            {userInfo.photos.map((item, key) => (
+              <SwiperItem key={key} data={item}>
+                <SwipperImage source={{uri: item.url}} resizeMode="cover" />
+              </SwiperItem>
+            ))}
+          </Swipper>
+        ) : (
+          <EmptyHeader></EmptyHeader>
+        )}
+        <PageBody>
+          <UserInfoArea>
+            <UserAvatar source={{uri: userInfo.avatar}} />
+            <UserInfo>
+              <UserName>{userInfo.name}</UserName>
+              <Stars stars={userInfo.stars} showNumber={true} />
+            </UserInfo>
+            <FavoriteButtom>
+              <FavoriteIcon
+                width="24"
+                height="24"
+                fill="#ff0000"
+                style={{marginTop: -10, marginLeft: 40}}
+              />
+            </FavoriteButtom>
+          </UserInfoArea>
+          <ServiceArea></ServiceArea>
+          <TestimonialArea></TestimonialArea>
+        </PageBody>
+      </Scroller>
     </Container>
   );
 };
--- src/screens/ProfessionalInfo/styles.js
+++ src/screens/ProfessionalInfo/styles.js
@@ -2,8 +2,80 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
-  background-color: #63c2d1;
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: #ffffff;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+`;
+
+export const EmptyHeader = styled.View``;
+
+export const PageBody = styled.View`
+  background-color: #ffffff;
+  border-top-left-radius: 50px;
+  margin-top: -50px;
+  min-height: 400px;
+`;
+
+export const UserInfoArea = styled.View`
+  flex-direction: row;
+`;
+
+export const ServiceArea = styled.View``;
+
+export const TestimonialArea = styled.View``;
+
+export const SwipeDot = styled.View`
+  width: 10px;
+  height: 10px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  margin: 3px;
+`;
+
+export const SwipeDotActive = styled.View`
+  width: 10px;
+  height: 10px;
+  background-color: #000000;
+  border-radius: 5px;
+  margin: 3px;
+`;
+
+export const SwiperItem = styled.View`
+  flex: 1;
+  background-color: #63c2d1;
+`;
+
+export const SwipperImage = styled.Image`
+  width: 100%;
+  height: 240px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 102px;
+  height: 102px;
+  border-radius: 12px;
+  margin-top: -30px;
+  margin-left: 40px;
+  border-width: 4px;
+  border-color: #ffffff;
+`;
+
+export const UserInfo = styled.View`
+  margin-left: 40px;
+`;
+
+export const UserName = styled.Text``;
+
+export const FavoriteButtom = styled.TouchableOpacity`
+  width: 30px;
+  height: 30px;
+  background-color: #ffffff;
+  border-radius: 15px;
 `;
